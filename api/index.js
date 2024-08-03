95a57c0db8e1a37ca8a2daa4a7184edf6dde7e14
Gregory Iakovlev <greg.yakovlev@hotmail.com>
2024-08-03 13:58:31 +0200

    Made Server, and parsing csv

--- api/index.js
+++ api/index.js
@@ -1,26 +1,22 @@
-
 import express from 'express';
-import routerAPI from './src/routes/user-routes.js'
-// import { https } from 'firebase-functions';
+import cors from 'cors';
+import routerAPI from './src/routes/user-routes.js';
 
 const port =  5000;
 
 const data=express();
 
+// Разрешаем запросы с любого домена (для разработки).
+// В продакшене укажите конкретные домены.
+data.use(cors());
 
 data.use(express.json());
-   data.use('/',routerAPI);
-
+data.use('/',routerAPI);
 
 data.get("/hello", (req, res) => {
   res.send("Hello human!");
 });
 
-
 data.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// export const api = https.onRequest(data);
-
-
