c68f8d13526e2dea684f7a62ea74370a0ddaf16d
Gregory Iakovlev <greg.yakovlev@hotmail.com>
2024-08-05 18:45:42 +0200

    q

--- api/index.js
+++ api/index.js
@@ -1,26 +1,15 @@
-
+import * as functions from 'firebase-functions';
 import express from 'express';
-import routerAPI from './src/routes/user-routes.js'
-import { https } from 'firebase-functions';
 
-const port =  5000;
+const app = express();
 
-const data=express();
-
-
-data.use(express.json());
-   data.use('/',routerAPI);
-
-
-data.get("/hello", (req, res) => {
-  res.send("Hello human!");
+app.get('/hello', (req, res) => {
+  res.send('Hello World 01!');
 });
 
+// Удалим прослушивание порта, так как Firebase Functions сами управляют этим
+// app.listen(PORT,()=>{
+//     console.log(`Server started on port ${PORT}`)
+// })
 
-data.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-export const api = https.onRequest(data);
-
-
+export const api = functions.https.onRequest(app);
